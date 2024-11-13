213aa409500596e5a248c5315a69a96748abca8f
FahdDewnology <42022327+FahdDewnology@users.noreply.github.com>
2024-11-13 16:21:58 +0530

    Delete First Upload.py

--- First Upload.py
+++
@@ -1,5 +0,0 @@
-Python 3.12.3 (v3.12.3:f6650f9ad7, Apr  9 2024, 08:18:48) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> #Upload a python file in GitHub Repository
->>> print("First Upload")
-First Upload
